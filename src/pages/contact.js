cb70b989d81cb00c44c5b45d806730e0388cb1e5
Jana Rajakumar <jana.stc@gmail.com>
2019-01-13 18:47:31 -0500

    Add sample contact page to test netlify form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,30 @@ import React from 'react'
 import {
   Layout
 } from '../components'
-import Typography from '@material-ui/core/Typography'
 
-const ContactPage = () => {
+const ContactPage = (props) => {
+  console.log(props)
+
   return (
     <Layout title='Contact' activePage='/contact/' >
-      <Typography>Hello</Typography>
+      <form method='post' action='/contact/' data-netlify='true' data-netlify-honeypot='bot-field'>
+        <div className='field half first'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' id='name' />
+        </div>
+        <div className='field half'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' name='email' id='email' />
+        </div>
+        <div className='field'>
+          <label htmlFor='message'>Message</label>
+          <textarea name='message' id='message' rows='6' />
+        </div>
+        <ul className='actions'>
+          <li><input type='submit' value='Send Message' className='special' /></li>
+          <li><input type='reset' value='Clear' /></li>
+        </ul>
+      </form>
     </Layout>
   )
 }
